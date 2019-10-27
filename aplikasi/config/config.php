a6d26c18077adfd06ff2b7557b571fabecac2e9d
irsyad <irsyadkh@gmail.com>
2019-10-27 20:08:12 +0700

    config

--- aplikasi/config/config.php
+++ aplikasi/config/config.php
@@ -2,6 +2,6 @@
 define('BASEURL','http://localhost/phpmvc/public');
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'phpmvc');
-define('DB_PASS', 'phpmvc');
+define('DB_PASS', 'phpmvcpass');
 
 ?>
