8aed2507a653938ebff0eefdfcb643f5146b45bb
irsyadkharisma <irsyadkh@gmail.com>
2019-10-28 01:21:39 +0700

    sanitize url

--- aplikasi/core/Aplikasi.php
+++ aplikasi/core/Aplikasi.php
@@ -1,14 +1,16 @@
 <?php
 
-class Aplikasi {
-    public function __construct(){
-        $url=$this->parse_url();
-        var_dump($url);
+class Aplikasi { #membuat class aplikasi
+    public function __construct(){ #membuat fungsi yang public dengan nama __construct/global function dari php
+        $url=$this->parse_url(); #mendefinisikan variable dengan nama 'url'
+        var_dump($url); 
     }
 
-    public function parse_url(){
+    public function parse_url(){ #membuat function publik dengan nama 'parse_url'
         if(isset($_GET['url'])){
-            $url = $_GET['url'];
+            $url = rtrim($_GET['url'],'/'); #right trim '/'
+            $url = filter_var($url, FILTER_SANITIZE_URL); #membersihkan url dari karakter aneh-aneh, biar secure
+            $url = explode('/', $url);
             return $url;
         }
     }
