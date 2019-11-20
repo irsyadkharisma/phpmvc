f521b8b2b7c72c82f176337b6d26a6d0e472af13
irsyadkharisma <irsyadkh@gmail.com>
2019-11-21 01:06:26 +0700

    clean all up

--- aplikasi/core/Controller.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Controller {
-   public function view($view, $data=[]){
-    require_once '../aplikasi/views/' . $view . '.php';
-   }
-}
-
-?>
--- aplikasi/init.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once 'core/Aplikasi.php';
-require_once 'core/Controller.php';
-
-?>
